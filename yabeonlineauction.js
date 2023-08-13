a5f2b085656249179f4e57b8fd8b65b65227dcca
Jeremy D. Opriasa <jdopriasa@WEB1465L-JDO.philweb01.ad.philweb.com.ph>
2023-08-13 20:48:20 +0800

    feat: Yabe Online Auction (Loops Exercise 2)

--- yabeonlineauction.js
+++ yabeonlineauction.js
@@ -1,51 +1,39 @@
 
-$('#submit').click(function(){
+var sale = 0.12;
+var quit = 0;  
+var weeks = 7;
+console.log("Number of weeks - " + weeks);
 
-  // console.log("Used Car");
+if(weeks != 0){
+  var calculateWeeks = 1; // control variable
+  var currentPrice = 0;
+  if(weeks == 10){
+    currentPrice = 0;
+    // weeks = 11;
+  }else{
 
-  var accountNumber = $('#accountNumber').val();
-  var firstName = $('#firstName').val();
-  var lastName = $('#lastName').val();
-  var purchasePrice = $('#purchasePrice').val();
-  var carPrice = 1000000;
+    var price = 10.00;
+    console.log("Price - " + price);
+    currentPrice = price - (price * 0.12);
+    // Calculate the input weeks to get the current price
+    while(calculateWeeks < weeks){
+      currentPrice = currentPrice - (currentPrice * 0.12);
+      // console.log("Current price - " + currentPrice);
+      // console.log("Calculate week increment - " + calculateWeeks);
 
-  var remainingBalance = carPrice - purchasePrice;
-  
-
-  
-  if(accountNumber == '' || firstName == '' || lastName == '' || purchasePrice == ''){
-    alert("Please fill-in everything.");  
-  }
-  else{
-    $('#result').append(`Account number: ${accountNumber}`);
-    $('#result').append(`<br>`);
-    $('#result').append(`Customer name: ${firstName} ${lastName}`);
-    $('#result').append(`<br>`);
-    $('#result').append(`Downpayment: ${purchasePrice}`);
-    $('#result').append(`<br>`);
-    
-    var twentyFourMonths = (remainingBalance / 24);
-    var withInterest = twentyFourMonths * 0.02;
-    var totalMonthly = twentyFourMonths + withInterest;
-    // * 0.02
-
-    var month = 24;
-    var cntrlVrbl = 0;
-    while(cntrlVrbl < month){
-      $('#result').append(`Price each month: ${parseInt(totalMonthly)}`);
-      $('#result').append(`<br>`);
-
-      if(cntrlVrbl == 23 && totalMonthly <= 25){
-        $('#result').append(`You may pay off the account.`);
-      }
-
-      cntrlVrbl = cntrlVrbl + 1;
+      calculateWeeks++;
     }
 
   }
-
-
   
+}
   
 
-})
+
+if(currentPrice == 0){
+  console.log("It's totally free!");
+} else{
+  console.log("Total price: " + currentPrice);
+}
+
+
